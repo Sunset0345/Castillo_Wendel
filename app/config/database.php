5e0a780afc427b0849476faab601f0e97c9f82ef
Sunset0345 <jamesbacsa2345@gmail.com>
2025-10-06 06:44:35 +0800

    j

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql1280048',
+    'username'	=> 'sql12800480',
     'password'	=> 'QqHeVQcx17' ,
-    'database'	=> 'sql1280048',
+    'database'	=> 'sql12800480',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
